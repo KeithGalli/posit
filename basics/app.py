113625dbfb1b6b2664acce341c549e5466e311d6
Keith Galli <k.galli603@gmail.com>
2024-04-29 12:00:27 -0400

    Added heatmap to shiny app

--- basics/app.py
+++ basics/app.py
@@ -6,13 +6,15 @@ import matplotlib.pyplot as plt
 from shiny import reactive
 from shiny.express import render, ui
 
-from shinywidgets import render_plotly
+from shinywidgets import render_plotly, render_widget
 import plotly.express as px
 
+import folium
+from folium.plugins import HeatMap
 
 @reactive.calc
 def dat():
-    infile = Path(__file__).parent / "data/sales_new.csv"
+    infile = Path(__file__).parent / "data/sales_with_coords.csv"
     return pandas.read_csv(infile)
 
 def apply_common_styles(fig):
@@ -23,10 +25,6 @@ def apply_common_styles(fig):
         'font': {'family': "Arial, sans-serif", 'size': 12, 'color': "darkblue"}
     })
     return fig
-
-with ui.navset_card_underline():
-    with ui.nav_panel("Sales over time"):
-        "ehlllo"
 
 with ui.navset_card_underline():
     with ui.nav_panel("Top Sellers"):
@@ -51,9 +49,24 @@ with ui.navset_card_underline():
 
             fig = px.bar(top_5_products, x="Product", y="Quantity Ordered")
 
-            fig.update_traces(marker_color=top_5_products["Quantity Ordered"], marker_colorscale='Blues')
+            fig.update_traces(marker_color=top_5_products["Quantity Ordered"], marker_colorscale='Reds')
             apply_common_styles(fig)
             return fig
+        
+with ui.navset_card_underline():
+    with ui.nav_panel("Sales Locations"):
+        @render.ui
+        def heatmap():
+            # Create a map centered roughly in the middle of the US
+            df = dat()
+            map = folium.Map(location=[37.0902, -95.7129], zoom_start=4)
+
+            # Prepare the data for the HeatMap; each entry in heatmap_data is (lat, lon, weight)
+            heatmap_data = [(lat, long, qty) for lat, long, qty in zip(df['lat'],df['long'], df['Quantity Ordered'])]
+            # Add HeatMap to the map
+            HeatMap(heatmap_data).add_to(map)
+
+            return map
 
 with ui.navset_card_underline():
     with ui.nav_panel("Data frame"):
