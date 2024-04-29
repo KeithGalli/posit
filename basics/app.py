518f7df3bd7b2a86015a0e84458d3a6984aa3873
Keith Galli <k.galli603@gmail.com>
2024-04-28 21:52:04 -0400

    A more impressive app

--- basics/app.py
+++ basics/app.py
@@ -15,19 +15,45 @@ def dat():
     infile = Path(__file__).parent / "data/sales_new.csv"
     return pandas.read_csv(infile)
 
-@render.plot
-def total_orders():
-    df = dat()
-    top_5_products = df.groupby("Product")["Quantity Ordered"].sum().nlargest(5)
-    top_5_products.plot(kind="bar")
-
-@render_plotly
-def total_orders2():
-    df = dat()
-    top_5_products = df.groupby("Product")["Quantity Ordered"].sum().largest(5)
-
-    fig = px.bar(top_5_products, x=top_5_products.index, y="Quantity Ordered")
+def apply_common_styles(fig):
+    # Set the background to transparent
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(0,0,0,0)',  # Transparent plot background
+        'paper_bgcolor': 'rgba(0,0,0,0)',  # Transparent figure background
+        'font': {'family': "Arial, sans-serif", 'size': 12, 'color': "darkblue"}
+    })
     return fig
+
+with ui.navset_card_underline():
+    with ui.nav_panel("Sales over time"):
+        "ehlllo"
+
+with ui.navset_card_underline():
+    with ui.nav_panel("Top Sellers"):
+        @render_plotly
+        def top_sellers():
+            df = dat()
+            top_5_products = df.groupby("Product")["Quantity Ordered"].sum().nlargest(5).reset_index()
+
+            print(top_5_products.head())
+
+            fig = px.bar(top_5_products, x="Product", y="Quantity Ordered")
+
+            fig.update_traces(marker_color=top_5_products["Quantity Ordered"], marker_colorscale='Blues')
+            apply_common_styles(fig)
+            return fig
+        
+    with ui.nav_panel("Lowest Sellers"):
+        @render_plotly
+        def lowest_sellers():
+            df = dat()
+            top_5_products = df.groupby("Product")["Quantity Ordered"].sum().nsmallest(5).reset_index()
+
+            fig = px.bar(top_5_products, x="Product", y="Quantity Ordered")
+
+            fig.update_traces(marker_color=top_5_products["Quantity Ordered"], marker_colorscale='Blues')
+            apply_common_styles(fig)
+            return fig
 
 with ui.navset_card_underline():
     with ui.nav_panel("Data frame"):
