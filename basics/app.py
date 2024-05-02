1ff81e7579c8d4b7968250c39ad23a839d444188
Keith Galli <k.galli603@gmail.com>
2024-05-02 08:34:08 -0400

    Updated to be single visualization for first video

--- basics/app.py
+++ basics/app.py
@@ -13,11 +13,10 @@ def dat():
     infile = Path(__file__).parent / "data/sales_with_coords.csv"
     return pd.read_csv(infile)
 
-ui.input_numeric("item_count", "Number of items to show", 1, min=1, max=20)  
-
 @render_plotly
 def top_sellers():
     df = dat()
-    top_5_products = df.groupby("product")["quantity_ordered"].sum().nlargest(input.item_count()).reset_index()
+    top_5_products = df.groupby("product")["quantity_ordered"].sum().nlargest(5).reset_index()
     fig = px.bar(top_5_products, x="product", y="quantity_ordered")
+    fig.update_layout(xaxis_title="Product", yaxis_title="Quantity Ordered")
     return fig
