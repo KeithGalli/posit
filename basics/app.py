e258999933dd482139823a72eb2a22748320e969
Keith Galli <k.galli603@gmail.com>
2024-05-21 21:19:07 -0400

    Reformatted with black again

--- basics/app.py
+++ basics/app.py
@@ -14,6 +14,7 @@ import seaborn as sns
 # Header Section with Logo and Title
 with ui.div(class_="header-container"):
     with ui.div(class_="logo-container"):
+
         @render.image
         def image():
             dir = Path(__file__).resolve().parent
@@ -23,7 +24,8 @@ with ui.div(class_="header-container"):
     with ui.div(class_="title-container"):
         ui.h2("Sales Dashboard - Part 5 of 5")
 
-ui.tags.style("""
+ui.tags.style(
+    """
     h2 {
         background-color: #5DADE2;
         color: white;
@@ -55,7 +57,9 @@ ui.tags.style("""
     body {
         background-color: #5DADE2;
     }
-""")
+"""
+)
+
 
 def apply_common_styles(fig):
     fig.update_layout(
@@ -64,15 +68,17 @@ def apply_common_styles(fig):
             "paper_bgcolor": "rgba(0,0,0,0)",
             "font": {"family": "Arial, sans-serif", "size": 12, "color": "darkblue"},
             "colorway": px.colors.sequential.Blues,
-            "xaxis_title": None
+            "xaxis_title": None,
         }
     )
     return fig
+
 
 @reactive.calc
 def dat():
     infile = Path(__file__).parent / "data/sales.csv"
     return pd.read_csv(infile)
+
 
 # Set common font and color palette for Altair
 _ = alt.themes.register(
@@ -94,7 +100,7 @@ _ = alt.themes.register(
             "background": "#FFFFFF",
             "font": "Arial",
         }
-    }
+    },
 )
 _ = alt.themes.enable("custom_theme")
 
@@ -125,11 +131,27 @@ with ui.card(full_screen=True):
             df["date"] = pd.to_datetime(df["order_date"])
             df["month"] = df["date"].dt.month_name()
             month_order = [
-                "January", "February", "March", "April", "May", "June",
-                "July", "August", "September", "October", "November", "December"
+                "January",
+                "February",
+                "March",
+                "April",
+                "May",
+                "June",
+                "July",
+                "August",
+                "September",
+                "October",
+                "November",
+                "December",
             ]
-            df["month"] = pd.Categorical(df["month"], categories=month_order, ordered=True)
-            counts = df.groupby(["month", "city"], observed=True)["quantity_ordered"].sum().reset_index()
+            df["month"] = pd.Categorical(
+                df["month"], categories=month_order, ordered=True
+            )
+            counts = (
+                df.groupby(["month", "city"], observed=True)["quantity_ordered"]
+                .sum()
+                .reset_index()
+            )
             city_counts = counts[counts["city"] == input.select()]
 
             chart = (
@@ -144,34 +166,55 @@ with ui.card(full_screen=True):
             )
             return chart
 
+
 with ui.layout_columns(widths=1 / 2):
     with ui.navset_card_underline(
-        footer=ui.input_numeric("items", "Number of Items", 5, min=1, max=10, step=None, width=0.5)
+        footer=ui.input_numeric(
+            "items", "Number of Items", 5, min=1, max=10, step=None, width=0.5
+        )
     ):
         with ui.nav_panel("Top Sellers"):
+
             @render_plotly
             def top_sellers():
                 df = dat()
-                top_5_products = df.groupby("product")["quantity_ordered"].sum().nlargest(input.items()).reset_index()
+                top_5_products = (
+                    df.groupby("product")["quantity_ordered"]
+                    .sum()
+                    .nlargest(input.items())
+                    .reset_index()
+                )
                 fig = px.bar(top_5_products, x="product", y="quantity_ordered")
-                fig.update_traces(marker_color=top_5_products["quantity_ordered"], marker_colorscale="Blues")
+                fig.update_traces(
+                    marker_color=top_5_products["quantity_ordered"],
+                    marker_colorscale="Blues",
+                )
                 fig.update_layout(yaxis_title="Quantity Ordered")
                 apply_common_styles(fig)
                 return fig
 
         with ui.nav_panel("Top Sellers Value ($)"):
+
             @render_plotly
             def top_sellers_value():
                 df = dat()
                 df["value"] = df["quantity_ordered"] * df["price_each"]
-                top_5_products = df.groupby("product")["value"].sum().nlargest(input.items()).reset_index()
+                top_5_products = (
+                    df.groupby("product")["value"]
+                    .sum()
+                    .nlargest(input.items())
+                    .reset_index()
+                )
                 fig = px.bar(top_5_products, x="product", y="value")
-                fig.update_traces(marker_color=top_5_products["value"], marker_colorscale="Blues")
+                fig.update_traces(
+                    marker_color=top_5_products["value"], marker_colorscale="Blues"
+                )
                 fig.update_layout(yaxis_title="Value ($)")
                 apply_common_styles(fig)
                 return fig
 
         with ui.nav_panel("Lowest Sellers"):
+
             @render_plotly
             def lowest_sellers():
                 df = dat()
@@ -183,12 +226,16 @@ with ui.layout_columns(widths=1 / 2):
                     .sort_values("quantity_ordered", ascending=False)
                 )
                 fig = px.bar(top_5_products, x="product", y="quantity_ordered")
-                fig.update_traces(marker_color=top_5_products["quantity_ordered"], marker_colorscale="Reds")
+                fig.update_traces(
+                    marker_color=top_5_products["quantity_ordered"],
+                    marker_colorscale="Reds",
+                )
                 fig.update_layout(yaxis_title="Quantity Ordered")
                 apply_common_styles(fig)
                 return fig
 
         with ui.nav_panel("Lowest Sellers Value ($)"):
+
             @render_plotly
             def lowest_sellers_value():
                 df = dat()
@@ -201,13 +248,16 @@ with ui.layout_columns(widths=1 / 2):
                     .sort_values("value", ascending=False)
                 )
                 fig = px.bar(top_5_products, x="product", y="value")
-                fig.update_traces(marker_color=top_5_products["value"], marker_colorscale="Reds")
+                fig.update_traces(
+                    marker_color=top_5_products["value"], marker_colorscale="Reds"
+                )
                 fig.update_layout(yaxis_title="Value ($)")
                 apply_common_styles(fig)
                 return fig
 
     with ui.navset_card_underline():
         with ui.nav_panel("Number of Orders by Hour of Day"):
+
             @render.plot
             def time_heatmap():
                 df = dat()
@@ -230,16 +280,25 @@ with ui.layout_columns(widths=1 / 2):
                     yticklabels=[f"{i}:00" for i in range(24)],
                 )
                 plt.yticks(color="darkblue")
-                plt.ylabel("Hour of Day", fontname="Arial", color="darkblue", fontsize=12)
-                plt.xlabel("Order Count", fontname="Arial", color="darkblue", fontsize=12)
+                plt.ylabel(
+                    "Hour of Day", fontname="Arial", color="darkblue", fontsize=12
+                )
+                plt.xlabel(
+                    "Order Count", fontname="Arial", color="darkblue", fontsize=12
+                )
+
 
 with ui.navset_card_underline():
     with ui.nav_panel("Sales Locations"):
+
         @render.ui
         def heatmap():
             df = dat()
             map = folium.Map(location=[37.0902, -95.7129], zoom_start=4)
-            heatmap_data = [(lat, long, qty) for lat, long, qty in zip(df["lat"], df["long"], df["quantity_ordered"])]
+            heatmap_data = [
+                (lat, long, qty)
+                for lat, long, qty in zip(df["lat"], df["long"], df["quantity_ordered"])
+            ]
             custom_gradient = {
                 0.0: "#E3F2FD",
                 0.2: "#BBDEFB",
@@ -251,8 +310,10 @@ with ui.navset_card_underline():
             HeatMap(heatmap_data, gradient=custom_gradient).add_to(map)
             return map
 
+
 with ui.navset_card_underline():
     with ui.nav_panel("Dataframe Sample"):
+
         @render.data_frame
         def frame():
             return dat().head(1000)
